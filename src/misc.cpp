0d2779c9e3178bf755443525d64c1e422780f3e3
NikitaG <nrg1@tpu.ru>
2022-05-18 09:48:36 +0700

    move from header to cpp

--- src/misc.cpp
+++ src/misc.cpp
@@ -1,1 +1,16 @@
 #include "misc.h"
+
+// maximim 233 ms
+void waitMs(uint32_t ms)
+{
+    // clear timer
+    SysTick->VAL = 0;
+    // set reload value
+    SysTick->LOAD = ms * (SYS_FREQ / 1000);
+    // start timer
+    SysTick->CTRL = SysTick_CTRL_ENABLE |
+                    SysTick_CTRL_CLKSOURCE;
+    // wait for timer
+    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG))
+        ;
+}
--- src/misc.h
+++ src/misc.h
@@ -3,17 +3,4 @@
 
 const uint32_t SYS_FREQ = 72e6;
 
-// maximim 233 ms
-void waitMs(uint32_t ms)
-{
-    // clear timer
-    SysTick->VAL = 0;
-    // set reload value
-    SysTick->LOAD = ms * (SYS_FREQ / 1000);
-    // start timer
-    SysTick->CTRL = SysTick_CTRL_ENABLE |
-                    SysTick_CTRL_CLKSOURCE;
-    // wait for timer
-    while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG))
-        ;
-}
+void waitMs(uint32_t ms);
