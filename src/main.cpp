d45624f0c6aea6be97d088d010b0820b46ba35d4
NikitaG <nrg1@tpu.ru>
2022-05-19 12:37:41 +0700

    move objects to heap

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,17 @@
 #include "timers.h"
 #include "keymatrix.h"
 
-
 int main()
 {
     rcc::setClock(rcc::RCCPort::gpioa, rcc::ClockMode::On);
 
-    gpio::Pin out_pins[3] = {{GPIOA, 0, gpio::GPIOMode::out_OpenDrain_50MHz},
-                             {GPIOA, 1, gpio::GPIOMode::out_OpenDrain_50MHz},
-                             {GPIOA, 2, gpio::GPIOMode::out_OpenDrain_50MHz}};
-    gpio::Pin in_pins[3] = {{GPIOA, 3, gpio::GPIOMode::in_Floating},
-                            {GPIOA, 4, gpio::GPIOMode::in_Floating},
-                            {GPIOA, 5, gpio::GPIOMode::in_Floating}};
-    uint8_t keys_array[3 * 3];
+    gpio::Pin *out_pins = new gpio::Pin[3]{{GPIOA, 0, gpio::GPIOMode::out_OpenDrain_50MHz},
+                                           {GPIOA, 1, gpio::GPIOMode::out_OpenDrain_50MHz},
+                                           {GPIOA, 2, gpio::GPIOMode::out_OpenDrain_50MHz}};
+    gpio::Pin *in_pins = new gpio::Pin[3]{{GPIOA, 3, gpio::GPIOMode::in_Floating},
+                                          {GPIOA, 4, gpio::GPIOMode::in_Floating},
+                                          {GPIOA, 5, gpio::GPIOMode::in_Floating}};
+    uint8_t *keys_array = new uint8_t[3 * 3];
 
     KeyMatrix::init(out_pins, 3, in_pins, 3, keys_array);
 
